d90be676ddf19d9042c6abe0ff351b763276cb7f
qianxinwei <17859509401@163.com>
2023-09-18 17:16:14 +0800

    版本2发布支持传入变量名和格式化

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
+#!/usr/bin/env node
 let iptFile = process.argv[2] || './1.xlsx'
 let outFile = process.argv[3] || './1.js'
+let dataName = process.argv[4] || 'newData'
+let dataFormate = process.argv[5] || 4;
+dataFormate =  Number(dataFormate)
+console.log(dataFormate)
 import parseXlsx from 'excel';
 import fs from 'fs'
 const xls = parseXlsx.default
 xls(iptFile).then((data) => {
   
-   fs.writeFile(outFile,'let newData = '+ JSON.stringify(data,null,2),(err)=>{
+   fs.writeFile(outFile,`let ${dataName} = `+ JSON.stringify(data,null,dataFormate),(err)=>{
     if(err) return console.log('写入失败')
     console.log('写入成功')
    })
